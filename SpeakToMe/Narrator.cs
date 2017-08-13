9dafa6f08f6a1a9e53d221371554e514a6adbfed
Jimmy Howe <jimmyhowe@live.co.uk>
2017-08-13 20:40:07 +0100

    Add SpeakToMe library

---
+++ SpeakToMe/Narrator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+
+namespace SpeakToMe
+{
+    public class Narrator
+    {
+        SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+
+        public void saySomething(String message)
+        {
+            speechSynthesizer.Speak(message);
+        }
+    }
+}
